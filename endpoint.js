366fa56e3d9cc27f65f342db7c29844504e6c9fc
LHebertdev <32530430+LHebertdev@users.noreply.github.com>
2017-10-05 12:51:01 -0500

    Update endpoint.js

--- endpoint.js
+++ endpoint.js
@@ -14,28 +14,15 @@
 
 // Make request to Github
 
-$("#button").click(function(){
-var username = $("#username").val();
-$.ajax({
-  url:'https://api.github.com/users/'+username,
-  data:{
-    client_id:'9ecc0f206ecd34f2f552',
-    client_secret:'6eee17df91630a531ea2acf49848dec408079e9c'
-  }
-}).done(function(user){
-  $.ajax({
-    url:'https://api.github.com/users/'+username+'/followers?per_page=5',
-    data:{
-      client_id:'9ecc0f206ecd34f2f552',
-      client_secret:'6eee17df91630a531ea2acf49848dec408079e9c'
-    }
-  }).done(function(followers){
-    $.each(followers, function(index, follower){
-      $('#returninfo here').append(`
-        <p>$username</p>
-        <p>$followers</p>
-      `);
-    });
-  });
+$("#button").click(function()(function ($) {
+    $('button').on('click', function () {
+        
+        // add spinner to indicate something is happening
+        $('<i class="fa fa-refresh fa-spin"/>').appendTo('#returninfohere');
  
-});
+        // get selected zip code from selectbox
+        var username = $('#username').val();
+ 
+        // make the AJAX request
+        $.getJSON('https://api.github.com/users/' + username +'followers')};
+                   };
